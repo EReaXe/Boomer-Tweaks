8b1de2a666acece534df0ef1adb27d03dd784ac9
EReaX <67808914+EReaXe@users.noreply.github.com>
2023-04-23 16:21:08 +0300

    Boomer Tweaks Beta Sürümü

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// // İndirme butonları için tıklama olayını dinleyin
+// var downloadButtons = document.querySelectorAll(".download-button");
+
+// downloadButtons.forEach(function (button) {
+//   button.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // İndirme işlemi burada yapılabilir
+//   });
+// });
